d92612cbc30a870f4542de258922d7bc982c3eaf
msa4090 <msa4090@naver.com>
2021-02-03 06:20:29 +0900

    .

--- BIFF/index.js
+++ BIFF/index.js
@@ -24,17 +24,46 @@ function sslide__init() {
     });
 }
 
-function clickMovie__init() {
-    $('.t-container .today-movie-list .swiper-slide').click(function () {
+function tslide__init() {
+    var swiper = new Swiper('.sc-3', {
+        slidesPerView: 4.8,
+        spaceBetween: 40,
+        loop: true,
+        centeredSlides: true,
+        pagination: {
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 3300,
+            disableOnInteraction: false,
+        }
+    });
+}
+
+function click__init() {
+    $('.click .swiper-slide').click(function () {
         if ($(this).hasClass('active')) {
             $(this).removeClass('active');
         } else {
-            $('.t-container .today-movie-list .swiper-slide').removeClass('active');
+            $('.click .swiper-slide').removeClass('active');
+            $(this).addClass('active');
+        }
+    });
+
+    $('.quick-link').click(function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        } else {
             $(this).addClass('active');
         }
     });
 }
 
-clickMovie__init();
+click__init();
 todaymovie__init();
 sslide__init();
+tslide__init();
